37b2bfa34f4676bb1c6cf46adf319eae0a0356ef
Leechaemin12 <chaemin0601@naver.com>
2025-03-24 23:01:03 +0900

    1주차-3번번

--- radius.py
+++ radius.py
@@ -1,20 +1,8 @@
-radius=2.0
-print("원의 반지름",radius)
-print("원의 면적",3.14*radius*radius)
-print("원의 둘레",2.0*3.14*radius)
-
-print(str(100))
-print(str(123.5))
-print(str([1,2,3]))
 
 n = int(input("정수 n을 입력하세요: "))
-print("입력한 정수는:", n)
-
-width=40
-height=20
-print("area: ", width*height*1/2) 
-
-radius=11
-PI=3.142
-print("원의 둘레:",radius*2*PI)
-print("원의 면적:",radius*radius*PI)
+if n%2 == 0:
+    print("True")
+else: 
+    print("False")
+    print("False")
+  
